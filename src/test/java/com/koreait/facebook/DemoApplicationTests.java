b56b282fae99ad00da88548cb55ce0d6aeea626b
jaeeun <dino218@naver.com>
2021-06-22 14:40:42 +0900

    ??

--- src/test/java/com/koreait/facebook/DemoApplicationTests.java
+++ src/test/java/com/koreait/facebook/DemoApplicationTests.java
@@ -34,6 +34,5 @@ class DemoApplicationTests {
         assertNotEquals(val,val2);
         System.out.println("1:"+val);
         System.out.println("2:"+val2);
-        System.out.println("dd");
     }
 }
