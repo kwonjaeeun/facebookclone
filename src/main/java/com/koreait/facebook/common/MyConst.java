8acee2dda6689b2a46bb3d569c1feb783e59ae75
jaeeun <dino218@naver.com>
2021-06-29 16:10:43 +0900

    feed~ing

--- src/main/java/com/koreait/facebook/common/MyConst.java
+++ src/main/java/com/koreait/facebook/common/MyConst.java
@@ -4,4 +4,5 @@ import org.springframework.stereotype.Component;
 @Component
 public class MyConst {
     public final String PROFILE_LIST = "profileList";
+    public final String RESULT = "result";
 }
